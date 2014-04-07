dad6c7be2c3e8d11bbfc089a206254d2c5e05c48
Chris Thomas <christopher.thomas@cbac.co.uk>
2014-04-07 14:59:13 +0100

    CONTENT UPDATE
    MATHS STARTERS CONTENT UPDATE

--- eng/js/data.js
+++ eng/js/data.js
@@ -1,5 +1,5 @@
 var codropsEvents = {
-'MM-01-YYYY' : '<a href="#"><div class="question"><span style="font-size:24px; text-align:left;">(i)<math><semantics><mrow><msqrt><mrow><mn>11.56</mn></mrow></msqrt></mrow></semantics></math><br>(ii)<math style="font-size:54px;"><semantics><mrow><msup><mrow><mn>5.4</mn></mrow><mn>2</mn></msup></mrow></semantics></math></span></div><div class="answer">Answer will appear here!</div></a>',
+'MM-01-YYYY' : '<a href="#"><div class="question"><span style="font-size:24px; text-align:left;">(i)<math><semantics><mrow><msqrt><mrow><mn>11.56</mn></mrow></msqrt></mrow></semantics></math><br>(ii)<math style="font-size:54px;"><semantics><mrow><msup><mrow><mn>5.4</mn></mrow><mn>2</mn></msup></mrow></semantics></math></span></div><div class="answer"><img src="images/maths-test-01.png"><br>Answer will appear here!</div></a>',
 'MM-02-YYYY' : '<a href="#"><div class="question"><math><semantics><mrow><mo stretchy="false">(</mo><mn>5.4</mn><mo>&#x00D7;</mo><msup><mrow><mn>10</mn></mrow><mn>3</mn></msup><mo stretchy="false">)</mo><mo>&#x00D7;</mo><mo stretchy="false">(</mo><mn>3</mn><mo>&#x00D7;</mo><msup><mrow><mn>10</mn></mrow><mn>5</mn></msup><mo stretchy="false">)</mo></mrow></semantics></math></div><div class="answer">Answer will appear here!</div></a>',
 'MM-03-YYYY' : '<a href="#"><div class="question"><img src="images/Aut07_181_01_1a.png"><br>Geraint buys a pair of jeans for £22.49 and a polo shirt for £13.87.<br> Find the total cost of these two items.</div><div class="answer"><img src="images/Aut07_181_01_1a.png"  ><br>£22.49 + £13.87 = £36.36</div></a>',
 'MM-04-YYYY' : '<a href="#"><div class="question"><img src="images/Aut07_181_01_10.png"><br>The diagram above shows a right-angled triangle with one angle of 67°.</div><div class="answer"><img src="images/Aut07_181_01_10.png"  ><br>180° - 90° - 67° = 23°</div></a>',
@@ -15,18 +15,41 @@ var codropsEvents = {
 'MM-14-YYYY' : '<a href="#"><div class="question"><img src="images/cool-boots.png"><br>Convert 54 000 cm2 into m2.</div><div class="answer"><img src="images/cool-boots.png"><br>54000 ÷ 100 = 540. 540 ÷ 100 = 5.4 m². <br> With area we must divide by the scale factor (100) twice.</div></a>',
 
 
-'MM-15-YYYY' : '<a href="#"><div class="question"><br><table><tr><td>x</td><td>-2</td><td>-1</td><td>0</td><td>1</td><td>2</td><td>3</td></tr><tr><td><math><mrow><mi>y</mi><mo>=</mo><mn>3</mn><msup><mi>x</mi><mn>3</mn></msup><mtext>&#x00A0;</mtext><mo>&#x2212;</mo><mtext>&#x00A0;</text><mn>4</mn><mi>x</mi><mtext>&#x00A0;</mtext><mo>&#x2212;</mo><mtext>&#x00A0;</mtext><mn>10</mn></mrow></math></td><td>10</td><td></td><td>-10</td><td>-11</td><td>-6</td><td>5</td></tr></table></div><div class="answer">3 × (-1)² - 4 × -1 - 10 = 3 × 1 - -4 - 10 = 3 + 4 - 10 = -3</div></a>',
-'MM-16-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-17-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-18-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-19-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-20-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-21-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-22-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-23-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-24-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-25-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
-'MM-26-YYYY' : '<a href="#"><div class="question"><span style="font-size:54px; text-align:left;">(i)<math><semantics><mrow><msqrt><mrow><mn>11.56</mn></mrow></msqrt></mrow></semantics></math><br><br>(ii)<math style="font-size:54px;"><semantics><mrow><msup><mrow><mn>5.4</mn></mrow><mn>2</mn></msup></mrow></semantics></math></span></div><div class="answer">Answer will appear here!</div></a>',
+'MM-15-YYYY' : '<a href="#"><div class="question"><br><table><tr><td>x</td><td>-2</td><td>-1</td><td>0</td><td>1</td><td>2</td><td>3</td></tr><tr><td><math><mrow><mi>y</mi><mo>=</mo><mn>3</mn><msup><mi>x</mi><mn>3</mn></msup><mtext>&#x00A0;</mtext><mo>&#x2212;</mo><mtext>&#x00A0;</text><mn>4</mn><mi>x</mi><mtext>&#x00A0;</mtext><mo>&#x2212;</mo><mtext>&#x00A0;</mtext><mn>10</mn></mrow></math></td><td>10</td><td></td><td>-10</td><td>-11</td><td>-6</td><td>5</td></tr></table><br>The table shows some of the values of y = 3x2 – 4x – 10 for values of x between – 2 and 3.</div><div class="answer">3 × (-1)² - 4 × -1 - 10 = 3 × 1 - -4 - 10 = 3 + 4 - 10 = -3</div></a>',
+
+
+'MM-16-YYYY' : '<a href="#"><div class="question">Find the Value of <br><math display="block"><mrow><mfrac><mn>5</mn><mn>6</mn></mfrac><mo>&#x2212;</mo><mfrac><mn>1</mn><mn>4</mn></mfrac></mrow></math></div><div class="answer"><math display="block"><mrow><mfrac><mn>5</mn><mn>6</mn></mfrac><mo>&#x2212;</mo><mfrac><mn>1</mn><mn>4</mn></mfrac><mo>=</mo><mfrac><mrow><mn>10</mn></mrow><mrow><mn>12</mn></mrow></mfrac><mo>&#x2212;</mo><mfrac><mn>3</mn><mrow><mn>12</mn></mrow></mfrac><mo>=</mo><mfrac><mn>7</mn><mrow><mn>12</mn></mrow></mfrac></mrow></math></div></a>',
+
+
+'MM-17-YYYY' : '<a href="#"><div class="question"><img src="images/Aut07_184_05_3c.png"><br>Calculate, in mph, the speed of a motorcyclist who travels 154 miles in 3 hours, 30 minutes.</div><div class="answer"><img src="images/Aut07_184_05_3c.png"><br>154 ÷ 3.5 = 44 m.p.h.</div></a>',
+
+
+'MM-18-YYYY' : '<a href="#"><div class="question"><img src="images/Aut07_184_05_5b.png"><br>Find the size of the angle marked y.</div><div class="answer"><img src="images/Aut07_184_05_5b.png"><br>115° + 88° + 82° = 285°. 360° - 285° = 75°. 180° - 75° = 105°</div></a>',
+
+
+'MM-19-YYYY' : '<a href="#"><div class="question"><br>Write each of the following numbers in standard form.<br> (i) 0·0083 <br>(ii) 7 500000000</div><div class="answer"> <math display="block"><mrow><mo stretchy="false">(</mo><mi>i</mi><mo stretchy="false">)</mo><mtext>&#x00A0;</mtext><mn>4.98</mn><mtext>&#x00A0;</mtext><mi>x</mi><mtext>&#x00A0;</mtext><msup><mrow><mn>10</mn></mrow><mrow><msup><mrow></mrow><mrow><mn>7</mn><mtext>&#x00A0;&#x00A0;&#x00A0;</mtext></mrow></msup></mrow></msup><mtext>&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;&#x00A0;</mtext><mo stretchy="false">(</mo><mi>i</mi><mi>i</mi><mo stretchy="false">)</mo><mn>3</mn><mtext>&#x00A0;</mtext><mi>x</mi><mtext>&#x00A0;</mtext><mn>10</mn><msup><mtext>&#x00A0;</mtext><mrow><mo>-</mo><mn>6</mn></mrow></msup></mrow></math></div></a>',
+
+
+'MM-20-YYYY' : '<a href="#"><div class="question"><br>Find, in standard form, the value of<br><br> <math display="block"><mrow><mo stretchy="false">(</mo><mn>2.66</mn><mo>&#x00A0;</mo><mo>&#x00D7;</mo><msup><mrow>    <mn>10</mn></mrow><mn>9</mn></msup><mo stretchy="false">)</mo><mo>&#x00F7;</mo><mo stretchy="false">(</mo><mn>7.6</mn><mo>&#x00D7;</mo><msup><mrow><mn>10</mn></mrow><mrow><mo>&#x2212;</mo><mn>3</mn></mrow></msup><mo stretchy="false">)</mo></mrow></math></div><div class="answer"><math display="block"><mrow><mtext>8</mtext><msup><mrow><mtext>.846&#x00A0;x&#x00A0;10</mtext></mrow><mtext>9</mtext></msup></mrow></math></div></a>',
+
+
+'MM-21-YYYY' : '<a href="#"><div class="question"><br><math display="block"><mrow><mtext>2</mtext><mfrac><mn>1</mn><mn>4</mn></mfrac><mo>&#x00D7;</mo><mn>3</mn><mfrac><mn>1</mn><mn>3</mn></mfrac></mrow></math></div><div class="answer">7½ ("The fraction 90/12 can be simplified to give 15/2, or 7½")</div></a>',
+
+
+'MM-22-YYYY' : '<a href="#"><div class="question"><math display="block"><mrow><msup><mn>8</mn><mn>0</mn></msup></mrow></math><br>Simplify the equation above.</div><div class="answer">Any number to the power 0 is equal to 1.</div></a>',
+
+
+'MM-23-YYYY' : '<a href="#"><div class="question"><br><br>There are 100 pupils in Year 9. The time taken by each pupil to answer a mental mathematics<br><img src="images/Aut07-184-09-19a.png"><br>Find an estimate of this distribution</div><div class="answer"><br><img src="images/Aut07-184-09-19a.png"><br>100 ÷ 2 = 50.<br> There are 50 pupils in the first three groups, and 50 in the final two groups.<br> The median is therefore 30 seconds.</div></a>',
+
+
+'MM-24-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"><br>Find 40% of £120.</div><div class="answer"><img src="images/money-notes-uk.jpg"><br>10% of £120 = £12. So 40% of £120 is   4 × £12 = £48.</div></a>',
+
+
+'MM-25-YYYY' : '<a href="#"><div class="question"><br><math display="block"><mrow><msup><mrow><mo stretchy="false">(</mo><mn>0.5</mn><mo stretchy="false">)</mo></mrow><mn>2</mn></msup></mrow></math><br>Write down the value of the equation above<br></div><div class="answer"><math display="block"><mrow><msup><mrow><mo stretchy="false">(</mo><mn>0.5</mn><mo stretchy="false">)</mo></mrow><mn>2</mn></msup><mtext>=&#x00A0;0</mtext><mtext>.5</mtext><mo>&#x00D7;</mo><mn>0.5</mn><mo stretchy="false">(</mo><mfrac>  <mn>1</mn><mn>2</mn></mfrac><mo>&#x00D7;</mo><mfrac><mn>1</mn><mn>2</mn></mfrac><mo>=</mo><mfrac><mn>1</mn><mn>4</mn>  </mfrac><mo stretchy="false">)</mo></mrow></math></div></a>',
+
+
+
+'MM-26-YYYY' : '<a href="#"><div class="question"></div><div class="answer">Answer will appear here!</div></a>',
 'MM-27-YYYY' : '<a href="#"><div class="question"><math style="font-size:54px;"> <semantics><mrow><msqrt><mrow><mn>11.56</mn></mrow></msqrt></mrow></semantics></math></div><div class="answer">Answer will appear here!</div></a>',
 'MM-28-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"  ><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
 'MM-29-YYYY' : '<a href="#"><div class="question"><img src="images/money-notes-uk.jpg"  ><br>Bronwen has £45 to spend on DVDs. Each DVD costs £8.49.<br><br>How many DVDs can she buy?</div><div class="answer">£45</div></a>',
